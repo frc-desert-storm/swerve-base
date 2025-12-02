c82f5612c49c11130ea5b7d2bd3ce417b9efbb61
nathana10988 <n.adams@10988.org>
2025-12-02 14:03:55 -0800

    Remove example project subsystem and commands

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -6,11 +6,8 @@ package frc.robot;
 
 import edu.wpi.first.math.MathUtil;
 import frc.robot.Constants.OperatorConstants;
-import frc.robot.commands.Autos;
-import frc.robot.commands.ExampleCommand;
 import frc.robot.commands.SwerveDriveCommand;
 import frc.robot.commands.SwerveXCommand;
-import frc.robot.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
