a8069fdc075a52ad518b2735266e1fe862c75b4d
Nathan <>
2025-12-09 15:17:23 -0700

    Add pid and feedforwards to both motors

--- src/main/java/frc/robot/subsystems/SwerveDriveModule.java
+++ src/main/java/frc/robot/subsystems/SwerveDriveModule.java
@@ -5,6 +5,7 @@ import com.ctre.phoenix6.hardware.TalonFX;
 
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.controller.SimpleMotorFeedforward;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
 import edu.wpi.first.units.Units;
@@ -18,7 +19,11 @@ public class SwerveDriveModule {
 
     private final CANcoder m_steerEncoder;
 
-    private final PIDController pid = new PIDController(.3, 0, 0);
+    private final SimpleMotorFeedforward m_steerFeedforward = new SimpleMotorFeedforward(0, 0, 0);
+    private final PIDController m_steerPid = new PIDController(.3, 0, 0);
+  
+    private final SimpleMotorFeedforward m_driveFeedforward = new SimpleMotorFeedforward(0, 0, 0);
+    private final PIDController m_drivePid = new PIDController(.3, 0, 0);
 
     public SwerveDriveModule(SwerveDriveConstants.SwerveModuleConstants constants) {
         m_driveMotor = new TalonFX(constants.DriveCanId, kCanBus);
@@ -29,21 +34,35 @@ public class SwerveDriveModule {
     public Rotation2d getCurrentAngle(){
         return Rotation2d.fromRadians(m_steerEncoder.getAbsolutePosition().getValue().in(Radian));
     }
+    
+    public SwerveModuleState getState(){
+        double speedMetersPerSecond = (m_driveMotor.getVelocity().getValue().in(Units.RPM) / 60.0)
+                * SwerveDriveConstants.kWheelCircumferenceMeters
+                * SwerveDriveConstants.kDriveGearRatio;
+
+        Rotation2d angle = getCurrentAngle();
+
+        return new SwerveModuleState(speedMetersPerSecond, angle);
+    }
 
     public void setDesiredState(SwerveModuleState desiredState){
-        Rotation2d currentAngle = getCurrentAngle();
-
         SwerveModuleState optimized = new SwerveModuleState(
                 desiredState.speedMetersPerSecond,
                 desiredState.angle
         );
+        
+        SwerveModuleState currentState = getState();
 
-        optimized.optimize(currentAngle);
-
-        double driveOutput = optimized.speedMetersPerSecond / SwerveDriveConstants.kMaxSpeedMetersPerSecond;
+        optimized.optimize(currentState.angle);
+        
+        double driveOutput = m_drivePid.calculate(currentState.speedMetersPerSecond, optimized.speedMetersPerSecond)
+                + m_driveFeedforward.calculate(optimized.speedMetersPerSecond);
 
         m_driveMotor.set(driveOutput);
         
-        m_steerMotor.set(pid.calculate(m_steerEncoder.getAbsolutePosition().getValue().in(Radian),optimized.angle.getRadians()));
+        double steerOutput = m_steerPid.calculate(currentState.angle.getRadians(), optimized.angle.getRadians())
+                + m_steerFeedforward.calculate(optimized.angle.getRadians());
+        
+        m_steerMotor.set(m_steerPid.calculate(m_steerEncoder.getAbsolutePosition().getValue().in(Radian),optimized.angle.getRadians()));
     }
 }
